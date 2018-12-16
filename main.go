7f98a542d0c8e699302abc5b087ce2d40953c34b
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-16 10:22:47 +0700

    closure function

--- main.go
+++ main.go
@@ -14,8 +14,21 @@ func main() {
 	println("Hello, " + bank.Title, func() string {
 		return "KTB"
 	}())
+
+	counter := counterFactory()
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
 }
 
 func init() {
 	fmt.Println("Initial 2")
+}
+
+func counterFactory() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
 }
