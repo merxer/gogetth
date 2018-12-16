92b6ce26058607905cede4da3f046cd4f1ca0596
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-16 10:51:55 +0700

    exercise

---
+++ combine_string.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func combineString(strs ...string) string {
+	var result string
+	for _, str := range strs {
+		if result != "" {
+			result = result + "," + str
+		} else {
+			result = str
+		}
+	}
+	return result
+}
+
+func swapString(first, second string) (string, string) {
+	return second, first
+}
+
+func main() {
+	fmt.Println(combineString("Hello", "World", "Third"))
+	fmt.Println(swapString("A", "B"))
+}
