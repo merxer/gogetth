ccd948bb222c1bb07c20b657a08d34d37beeaf99
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-16 10:27:18 +0700

    variadic function

--- main.go
+++ main.go
@@ -5,30 +5,6 @@ import (
 
 )
 
-func init() {
-	fmt.Println("Initial")
-}
-
 func main() {
-
-	counter := counterFactory()
-	printCount(counter)
-	printCount(counter)
-	printCount(counter)
-}
-
-func init() {
-	fmt.Println("Initial 2")
-}
-
-func counterFactory() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
-	}
-}
-
-func printCount(fn func() int ) {
-	fmt.Println(fn())
+	fmt.Println("Hello", "World")
 }
